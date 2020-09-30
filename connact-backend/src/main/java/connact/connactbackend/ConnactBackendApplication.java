896f421bf8da88dd522f15069fbc099ff57c51a7
Dennis Sperber <dennissperber@live.nl>
2020-09-30 14:57:40 +0200

    Added back-end.

---
+++ connact-backend/src/main/java/connact/connactbackend/ConnactBackendApplication.java
@@ -0,0 +1,13 @@
+package connact.connactbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnactBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnactBackendApplication.class, args);
+	}
+
+}
---
+++ connact-backend/src/test/java/connact/connactbackend/ConnactBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package connact.connactbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnactBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
