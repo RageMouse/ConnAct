20e5e784aceb0498b5763e50e8c3ed5884ad85e6
Dennis Sperber <dennissperber@live.nl>
2020-10-16 12:19:34 +0200

    Made CreateEvent front-end, and added it to MyEvents view.

--- connact-frontend/src/components/CreateEvent.vue
+++ connact-frontend/src/components/CreateEvent.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container>
-    <v-card class="elevation-12">
+    <v-card class="elevation-12 mx-auto" max-width="1000px">
       <v-toolbar dark color="primary">
         <v-toolbar-title>Create Event</v-toolbar-title>
       </v-toolbar>
@@ -14,6 +14,74 @@
             label="Name"
             required
           ></v-text-field>
+
+          <v-textarea
+            id="eventnDescription"
+            v-model="eventDescription"
+            :counter="255"
+            :rules="[(v) => !!v || 'Description is required']"
+            label="Description"
+            required
+          ></v-textarea>
+
+          <v-menu
+            v-model="menu2"
+            :close-on-content-click="true"
+            :nudge-right="40"
+            transition="scale-transition"
+            offset-y
+            min-width="290px"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field
+                v-model="dateStart"
+                label="Start Date for event"
+                prepend-icon="mdi-calendar"
+                readonly
+                v-bind="attrs"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker
+              v-model="dateStart"
+              @input="menu2 = true"
+              :min="getNowDate"
+            ></v-date-picker>
+          </v-menu>
+
+          <v-menu
+            v-model="menu2"
+            :close-on-content-click="true"
+            :nudge-right="40"
+            transition="scale-transition"
+            offset-y
+            min-width="290px"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field
+                v-model="dateEnd"
+                label="End Date for event"
+                prepend-icon="mdi-calendar"
+                readonly
+                v-bind="attrs"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker
+              v-model="dateEnd"
+              @input="menu2 = true"
+              :min="dateStart"
+            ></v-date-picker>
+          </v-menu>
+
+          <v-btn
+            id="validateButton"
+            :disabled="!valid"
+            color="success"
+            class="mr-4"
+            @click="validate"
+            >Validate</v-btn
+          >
         </v-form>
       </v-card-text>
     </v-card>
@@ -24,13 +92,24 @@
 export default {
   data: () => ({
     eventName: "",
-    description: "",
+    nameRules: [
+      (v) => !!v || "Name is required",
+      (v) => (v && v.length <= 25) || "Name must be less than 25 characters",
+    ],
+    eventDescription: "",
     dateStart: "",
     dateEnd: "",
-    state: ""
+    state: "",
+    selectedDate: null,
   }),
 
-  methods: {
-  }
+  methods: {},
+
+  computed: {
+    getNowDate() {
+      var nowDate = new Date();
+      return nowDate.toISOString().slice(0, 10);
+    },
+  },
 };
 </script>
