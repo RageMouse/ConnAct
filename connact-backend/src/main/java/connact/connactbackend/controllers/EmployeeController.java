2d56cbf9916baf2a4b287f5bdd2b98cb9202c2e9
Dimitri <dimitri1997NL@hotmail.nl>
2020-11-02 14:38:33 +0100

    Encryptie toegevoegd register

--- connact-backend/src/main/java/connact/connactbackend/controllers/EmployeeController.java
+++ connact-backend/src/main/java/connact/connactbackend/controllers/EmployeeController.java
@@ -13,6 +13,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping(value="/employee")
@@ -20,26 +26,51 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmployeeController {
     @Autowired
     private EmployeeRepo employeeRepo;
+    private byte[] saltG;
 
 
     @PostMapping("/")
-    public ResponseEntity<?> createAuction(@RequestBody EmployeeCreateModel employeeCreateModel) {
-        System.out.println("WORDT DIE EVENT AANGEROEPEN?");
-        System.out.println("raar ding");
-        if(employeeCreateModel!=null){
-            System.out.println("hij maakt wel?");
-            System.out.println("naam  :"+ employeeCreateModel.getUserName()+"    Wachtwoord"+employeeCreateModel.getPassword());
-        }
-        else{
-            System.out.println("hij maakt niet?");
-        }
-        if (employeeCreateModel.getUserName() == null || employeeCreateModel.getPassword() == null) {
+    public ResponseEntity<?> createEmployee(@RequestBody EmployeeCreateModel employeeCreateModel) {
 
+        if (employeeCreateModel.getUserName() == null || employeeCreateModel.getPassword() == null) {
             return new ResponseEntity<Error>(HttpStatus.NO_CONTENT);
         }
-        System.out.println("Deze lijn start");
-        Employee employee = new Employee(employeeCreateModel.getUserName(), employeeCreateModel.getPassword());
+
+        //create salt and create has for password
+        saltG = createsalt();
+        employeeCreateModel.setSalt(saltG);
+        String generatedHashPassword = hash(employeeCreateModel.getPassword(),saltG);
+
+        Employee employee = new Employee(employeeCreateModel.getUserName(), generatedHashPassword,employeeCreateModel.getSalt());
         employeeRepo.save(employee);
         return new ResponseEntity<>(employee, HttpStatus.CREATED);
+    }
+    public String hash(String passwordToHash, byte[] salt){
+        String generatedPassword = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+            md.update(salt);
+            byte[] bytes = md.digest(passwordToHash.getBytes(StandardCharsets.UTF_8));
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i< bytes.length ;i++){
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            generatedPassword = sb.toString();
+        }
+        catch (NoSuchAlgorithmException e){
+            e.printStackTrace();
+        }
+        return generatedPassword;
+    }
+
+    /*public boolean checkPassword(String hash, byte[] salt){
+        String generatedHash = hash(hash, salt);
+        return hashh.equals(generatedHash);
+    }*/
+    public byte[] createsalt(){
+        SecureRandom random = new SecureRandom();
+        saltG = new byte[16];
+        random.nextBytes(saltG);
+        return saltG;
     }
 }
--- connact-backend/src/main/java/connact/connactbackend/models/EmployeeCreateModel.java
+++ connact-backend/src/main/java/connact/connactbackend/models/EmployeeCreateModel.java
@@ -6,4 +6,6 @@ import lombok.Data;
 public class EmployeeCreateModel {
     private String userName;
     private String password;
+    private String test;
+    private byte[] salt;
 }
