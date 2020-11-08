3a39167f16586199ee5863c59904b53f9499d06f
DSperber <dennissperber@live.nl>
2020-11-08 14:31:41 +0100

    Update CreateEvent.vue

--- connact-frontend/src/components/CreateEvent.vue
+++ connact-frontend/src/components/CreateEvent.vue
@@ -107,7 +107,7 @@ export default {
   methods: {
     send: function () {
       this.axios
-        .post("http://192.168.178.21:8089/event/", {
+        .post("http://192.168.99.100:8089/event/", {
           eventName: this.eventName,
           eventDescription: this.eventDescription,
           dateStart: this.dateStart,
