bfdac0b6e5390725a5b61d1dc84011ccf19db280
Dennis Sperber <dennissperber@live.nl>
2020-11-15 16:15:27 +0100

    Added backend for getting myEvents

--- connact-backend/src/main/java/connact/connactbackend/repositories/EventRepo.java
+++ connact-backend/src/main/java/connact/connactbackend/repositories/EventRepo.java
@@ -2,8 +2,12 @@ package connact.connactbackend.repositories;
 
 import connact.connactbackend.entities.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface EventRepo extends JpaRepository<Event, Long> {
+    List<Event> findEventsByOwner(@Param("employeeId")String employeeId);
 }
