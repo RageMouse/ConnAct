e215d8cc9c671d3f5ecb3b3bd91ebfd2a7588cd0
Dennis Sperber <dennissperber@live.nl>
2020-11-21 16:23:47 +0100

    Added backend stuff

--- connact-backend/src/main/java/connact/connactbackend/controllers/EventController.java
+++ connact-backend/src/main/java/connact/connactbackend/controllers/EventController.java
@@ -1,12 +1,19 @@
 package connact.connactbackend.controllers;
 
 import connact.connactbackend.entities.Event;
+import connact.connactbackend.entities.Interest;
+import connact.connactbackend.entities.Profile;
+import connact.connactbackend.entities.Skill;
 import connact.connactbackend.models.EventCreateModel;
+import connact.connactbackend.models.EventEditModel;
+import connact.connactbackend.models.ProfileEditModel;
 import connact.connactbackend.repositories.EventRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -34,5 +41,18 @@ public class EventController {
     @GetMapping("/{userId}")
     public Iterable<Event> events(@PathVariable("userId") Long id) {
         return eventRepo.findEventsByOwnerId(id);
+    }
+
+    @PutMapping(path = "/")
+    public ResponseEntity<?> editEvent(@RequestBody EventEditModel eventEditModel){
+        Event event = eventRepo.getOne(eventEditModel.getEventId());
+
+        event.setEventName(eventEditModel.getEventName());
+        event.setDescription(eventEditModel.getEventDescription());
+        event.setDateStart(eventEditModel.getDateStart());
+        event.setDateEnd(eventEditModel.getDateEnd());
+
+        eventRepo.save(event);
+        return new ResponseEntity<>(event, HttpStatus.ACCEPTED);
     }
 }
